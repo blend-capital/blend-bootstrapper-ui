f2840bc41e8a65aa2206e356463af789fb837e01
Ryang-21 <ryanyang2001@gmail.com>
2024-06-12 14:31:07 -0600

    change radio buttons to square buttons

--- src/components/common/SelectableInput.tsx
+++ src/components/common/SelectableInput.tsx
@@ -2,21 +2,41 @@ import { useBootstrapper } from '../../hooks/bootstrapContext';
 export function SelectableInput({ inputName }: { inputName: string }) {
   const { bootstrapperId, selectedOption, setSelectedOption, setId } = useBootstrapper();
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(e.target.value);
+    setId(undefined);
+  };
+
+  const isSelected = selectedOption === inputName;
+
   return (
-    <div style={{ display: 'flex', alignItems: 'center', marginRight: '20px' }}>
+    <label
+      style={{
+        display: 'inline-block',
+        padding: '10px',
+        margin: '5px',
+        border: '1px solid',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        width: '100px',
+        background: isSelected
+          ? 'linear-gradient(45deg, #FDDC5C, #FDDC5C 50%, #FDDC5C 50%, #FDDC5C)'
+          : '#242424',
+        color: isSelected ? '#242424' : '#FDDC5C',
+      }}
+    >
       <input
         type="radio"
-        id="option1"
-        name="option"
+        name="action"
         value={inputName}
-        checked={selectedOption === inputName}
         disabled={bootstrapperId == undefined || bootstrapperId == ''}
-        onChange={(e) => {
-          setSelectedOption(e.target.value);
-          setId(undefined);
+        checked={isSelected}
+        onChange={handleChange}
+        style={{
+          display: 'none',
         }}
       />
-      <label htmlFor="option1">{inputName}</label>
-    </div>
+      {inputName}
+    </label>
   );
 }
